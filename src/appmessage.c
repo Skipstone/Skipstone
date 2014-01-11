4a2b78b4a37175b6527bdc9b1b16ea829a2e91c4
Neal <neal@ineal.me>
2014-01-11 16:28:03 -0600

    Register for AppMessage callbacks before opening a connection as suggested by the documentation.

--- src/appmessage.c
+++ src/appmessage.c
@@ -13,11 +13,11 @@ static void out_sent_handler(DictionaryIterator *sent, void *context);
 static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context);
 
 void appmessage_init(void) {
-	app_message_open(128 /* inbound_size */, 64 /* outbound_size */);
 	app_message_register_inbox_received(in_received_handler);
 	app_message_register_inbox_dropped(in_dropped_handler);
 	app_message_register_outbox_sent(out_sent_handler);
 	app_message_register_outbox_failed(out_failed_handler);
+	app_message_open(128 /* inbound_size */, 64 /* outbound_size */);
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
